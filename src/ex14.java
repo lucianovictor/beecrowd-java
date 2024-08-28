8ec648d25d80d4b449b8722b26668b5b69dde356
Luciano Victor <lucianocarvallho7@gmail.com>
2024-08-28 10:39:43 -0300

    adding exercise 14

---
+++ src/ex14.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ex14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int [] vet = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            vet[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            if (vet[i] < 0) {
+                System.out.println(vet[i]);
+            }
+        }
+        sc.close();
+    }
+}
